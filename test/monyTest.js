6519afd823f2ec74f30783452ac0841ace91a8ba
Abdullahi <abdullahi45230@student.simad.edu.so>
2026-05-13 17:55:55 +0300

    create automated tests for formatCurrency

--- test/monyTest.js
+++ test/monyTest.js
@@ -1,5 +1,9 @@
 import {formatCurrency} from '../scripts/utils/money.js';
 
+console.log('test suite : formatCurrency');
+console.log ('converts cents into dollars');
+
+
 if (formatCurrency(2095) === '20.95')
 {
  console.log ('Test passed');
@@ -8,6 +12,7 @@ if (formatCurrency(2095) === '20.95')
 else{
  console.log ('Test failed');
 }
+console.log('works with 0');
 
 if (formatCurrency(0) === '0.00'){
 console.log("passed");
@@ -15,6 +20,7 @@ console.log("passed");
 else {
 console.log("failed");
 }
+console.log('rounds up to the nearest cent');
 
 
 if (formatCurrency( 2000.5) === '20.01'){
